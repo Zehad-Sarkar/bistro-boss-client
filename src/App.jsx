c12757994c5ac21d1af5ea277935d574013b1e5f
Zehad-Sarkar <zehadsarkar22@gmail.com>
2023-05-22 23:18:23 +0600

    bistro boss initialized and push on git hub with navbar and footer

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <button className="w-64 rounded-full btn">Button</button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/sharedPages/Navbar/Navbar.jsx
@@ -0,0 +1,89 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const navOptions = (
+    <>
+      <li>
+        <Link>Home</Link>
+      </li>
+      <li>
+        <Link>Contact us</Link>
+      </li>
+      <li>
+        <Link>DashBoard</Link>
+      </li>
+      <li>
+        <Link>Our Menu </Link>
+      </li>
+      <li>
+        <Link>Our Shop</Link>
+      </li>
+      <li tabIndex={0}>
+        <a className="justify-between">
+          More
+          <svg
+            className="fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+          </svg>
+        </a>
+        <ul className="p-2">
+          <li>
+            <a>Submenu 1</a>
+          </li>
+          <li>
+            <a>Submenu 2</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a>SignOut</a>
+      </li>
+    </>
+  );
+  return (
+    <div className="fixed z-10 max-w-6xl mx-auto text-white bg-black navbar">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-5 h-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+          >
+            {navOptions}
+          </ul>
+        </div>
+        <Link to="/" className="text-xl normal-case btn btn-ghost">
+          Bistro Boss
+        </Link>
+      </div>
+      <div className="hidden navbar-center lg:flex">
+        <ul className="px-1 menu menu-horizontal">{navOptions}</ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn btn-primary">Get started</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
