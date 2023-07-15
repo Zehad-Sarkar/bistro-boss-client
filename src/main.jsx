259f5801ed810f0fe5bc224e6cc3156ad178aa92
Zehad-Sarkar <zehadsarkar22@gmail.com>
2023-07-15 12:47:00 +0600

    project impliment started now

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,9 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/Routes.jsx";
-import { HelmetProvider } from "react-helmet-async";
-import AuthProvider from "./provider/AuthProvider";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <div className="max-w-6xl mx-auto">
-          <RouterProvider router={router} />
-        </div>
-      </QueryClientProvider>
-    </HelmetProvider>
-  </AuthProvider>
+  <div className="max-w-6xl mx-auto">
+    <RouterProvider router={""} />
+  </div>
 );
